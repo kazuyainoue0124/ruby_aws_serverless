363f336a290d6133bf204aa669f8225cc21b7de6
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-11-04 12:18:35 +0900

    世界時計の実装

--- world-clock-api/world_clock/app.rb
+++ world-clock-api/world_clock/app.rb
@@ -1,38 +1,32 @@
-# require 'httparty'
 require 'json'
+require 'tzinfo'
+require 'logger'
+require 'uri'
+
+def logger
+  @logger ||= Logger.new($stdout, level: Logger::Severity::INFO)
+end
+
+def create_local_time(time_str, zone_abbreviation)
+  time = Time.parse(time_str)
+
+  zone = TZInfo::Timezone.all.find { _1.abbreviation == zone_abbreviation }
+  raise "Timezone not found: #{zone_abbreviation}" if zone.nil?
+
+  time.localtime(zone.observed_utc_offset).iso8601
+rescue ArgumentError
+  raise "Invalid time format: #{time_str}"
+end
 
 def lambda_handler(event:, context:)
-  # Sample pure Lambda function
+  logger.debug(event)
+  logger.debug(context)
 
-  # Parameters
-  # ----------
-  # event: Hash, required
-  #     API Gateway Lambda Proxy Input Format
-  #     Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+  params = URI.decode_www_form(event['body']).to_h
+  body = create_local_time(*params['text'].split(',').map(&:strip))
 
-  # context: object, required
-  #     Lambda Context runtime methods and attributes
-  #     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-
-  # Returns
-  # ------
-  # API Gateway Lambda Proxy Output Format: dict
-  #     'statusCode' and 'body' are required
-  #     # api-gateway-simple-proxy-for-lambda-output-format
-  #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-
-  # begin
-  #   response = HTTParty.get('http://checkip.amazonaws.com/')
-  # rescue HTTParty::Error => error
-  #   puts error.inspect
-  #   raise error
-  # end
-
-  {
-    statusCode: 200,
-    body: {
-      message: "Hello World!",
-      # location: response.body
-    }.to_json
-  }
+  { statusCode: 200, body: }
+rescue StandardError => e
+  logger.fatal(e.full_message)
+  { statusCode: 200, body: e.message }
 end
